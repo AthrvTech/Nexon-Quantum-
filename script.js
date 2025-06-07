497a94b833004ea5aa190bc24bd6c2452d688ef9
AthrvTech <mrhackershivansh143@gmail.com>
2025-06-08 05:23:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function toggleSidebar() {
-  const sidebar = document.getElementById('sidebar');
-  sidebar.classList.toggle('open');
-}
+// script.js
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
