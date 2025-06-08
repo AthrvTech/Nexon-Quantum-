1a831b5a35b943bd39eacccbea829644d223758c
AthrvTech <mrhackershivansh143@gmail.com>
2025-06-08 06:33:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// script.js
 
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
-
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
-});
