c5ed55cc287213d442d8671f3b6221b87460cc4b
AthrvTech <mrhackershivansh143@gmail.com>
2025-07-11 02:15:45 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.querySelector('nav ul');
-
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('nav-active');
-  menuToggle.classList.toggle('open');
-});
